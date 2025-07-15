87303215b0a4c50c4354465cd7a684d983993422
sanuarKhan <sanuarkhan220@gmail.com>
2025-07-15 12:33:26 +0600

    implement product management API with CRUD operations and MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import pruductRoutes from "./routes/product.route.js";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000");
+//middleware
+app.use(express.json());
+
+//routes
+app.use("/api/products", pruductRoutes);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server started at http://localhost:${PORT}`);
 });
