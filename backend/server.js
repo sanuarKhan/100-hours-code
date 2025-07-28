a43aeffe314ec5331b3f12c9af49ce961fe0e5d1
sanuarKhan <sanuarkhan220@gmail.com>
2025-07-28 14:14:14 +0600

    fix: correct productRoutes import and add dist directory to .gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { connectDB } from "./config/db.js";
-import pruductRoutes from "./routes/product.route.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
+// Modern ES modules way to get __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 5000;
+
 //middleware
 app.use(express.json());
 
 //routes
-app.use("/api/products", pruductRoutes);
+app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server started at http://localhost:${PORT}`);
